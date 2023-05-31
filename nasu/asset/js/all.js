39565a95b992c0be7dcef432591ef7a1f44d12a2
Liang <92705187+liangfromtaiwan@users.noreply.github.com>
2023-05-31 18:19:21 +0900

    Delete nasu directory

--- nasu/asset/js/all.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $('.l-header__hamburger').click(function (e) { 
-        e.preventDefault();
-        $('.l-header__hamburger').toggleClass('active');
-        $('.l-header__inner').toggleClass('active');
-    });
-});
